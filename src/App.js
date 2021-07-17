e14a43cb1cd3634621a3c4bf86a8bd3e408a3039
Lizaveta <lisa.bodunova@gmail.com>
2021-07-17 17:21:42 +0200

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import listSvg from './assets/img/list.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div className='todo'>
+    <div className='todo__sidebar'>
+      <ul className='todo__list'>
+        <li>
+          <i>
+            <img src={listSvg} alt='list icon' />
+          </i>
+          <span>Все задачи</span>
+        </li>
+      </ul>
     </div>
-  );
+    <div className='todo__tasks'></div>
+  </div>);
 }
 
 export default App;
